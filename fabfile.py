dac96090fa21a9eaf0b1fe45e972b21c91dd259b
okbm <freezemoon.s@gmail.com>
2015-01-19 00:02:34 +0900

    アップデート

--- fabfile.py
+++ fabfile.py
@@ -7,16 +7,15 @@ from cuisine import run
 import cuisine
 
 env.hosts = ['vagrant@192.168.56.101']
-
-# todo https://github.com/stephenmcd/mezzanine/blob/master/mezzanine/project_template/fabfile.py を参考に置き換える
+#env.port = 2222
 env.user = 'vagrant'
 env.password = 'vagrant'
+env.forward_agent = True
 
 @task
 def update_packages():
     puts(green('update packages'))
-    # TODO あとでコメントアウト解除する
-    # sudo("apt-get update")
+    sudo("apt-get update")
 
 # 使いそうなツール
 @task
@@ -41,10 +40,11 @@ def setup_packages():
     sudo ("ln -fs /home/vagrant /var/www")
 
     # php
-    sudo("add-apt-repository -y ppa:ondrej/php5")
+    sudo("add-apt-repository -y ppa:ondrej/php5-5.6")
+    sudo("apt-get update")
 
     packages = '''
-        libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-mcrypt php5-xdebug php5-cli php5
+        php5 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-mcrypt php5-xdebug php5-cli
         '''.split()
 
     for pkg in packages:
@@ -55,11 +55,10 @@ def setup_packages():
     cuisine.package_ensure('redis-server')
 
     # composer
-    #TODO あとでコメントアウト解除する
-    #run("curl -sS https://getcomposer.org/installer | php")
-    #sudo("mv composer.phar /usr/local/bin/composer")
-    #run('composer global require "phpunit/phpunit=4.3.*"')
-    #run('echo PATH="$PATH":~/.composer/vendor/bin/ >> ~/.bashrc')
+    run("curl -sS https://getcomposer.org/installer | php")
+    sudo("mv composer.phar /usr/local/bin/composer")
+    run('composer global require "phpunit/phpunit=4.3.*"')
+    run('echo PATH="$PATH":~/.composer/vendor/bin/ >> ~/.bashrc')
 
 @task
 def restart_application():
@@ -67,11 +66,40 @@ def restart_application():
     cuisine.upstart_ensure('apache2')
 
 @task
+def setup_original():
+    puts(green('setup original'))
+
+    # ssh
+    run('echo "Host github.com" > $HOME/.ssh/config');
+    run('echo "     HostName github.com" >> $HOME/.ssh/config');
+    run('echo "     User git" >> $HOME/.ssh/config');
+    run('echo "     StrictHostKeyChecking no" >> $HOME/.ssh/config');
+    run('chmod 600 $HOME/.ssh/config');
+
+    # git
+    run('git clone git@github.com:okbm/dotfiles.git');
+    run('WORK=$HOME/');
+
+    # mysql
+    # mysql -uroot -proot < $WORK/misc/local-create.sql
+
+@task
+def setup_tmp():
+    puts(green('tmp done'))
+#   確認用のコマンドをここで動かしてOKなら別のところに移動させる
+    # mysql
+    # mysql -uroot -proot < $WORK/misc/local-create.sql
+
+
+@task
 def main():
     update_packages()
     setup_devtools()
     setup_packages()
     restart_application()
+
+    setup_original()
+    setup_tmp()
 
     puts(green('finish script'))
 
